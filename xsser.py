030a4763ce2f70d1c6b94caa03830c0879652d55
r00t <th3location@gmail.com>
2013-09-19 23:11:11 +0200

    fixed the engine, added new libs, change the way of testing and using the app

--- xsser.py
+++ xsser.py
@@ -16,6 +16,8 @@ GNU General Public License for more details.
 
 from libs.crawler import scan
 from libs.crawler import injector
+from libs.common import *
+from libs.core.helpdesk import *
 import sys
 
 print "###########################################";
@@ -27,10 +29,48 @@ print ":::::::::::The Aut0Ma@tic XSS3r:::::::::::::"
 print ":.....::.....:..:..:..:.....::.....::.....:\n"
 
 if len(sys.argv) < 2:
-    print "usage: python xsser.py --target=\"http://victim.com?parameter=value\""
+    print "usage: python xsser.py --url=\"http://victim.com?parameter=value\"\nFor Help: can use python xsser.py --help for commands list and usage"
     sys.exit(0)
 
-userLink = sys.argv[1];
-GettingAllLinks = scan.getFromURL(userLink.replace("--target=",""));
-for Link in GettingAllLinks:
-    injector.RunAudit(Link)
+Doc = sys.argv[1];
+if Doc == "--help":
+    doc.docHelp()
+    sys.exit(0)
+
+Target = sys.argv[1];
+Type = sys.argv[2];
+TypeMe = Type.replace("--type=","")
+urlInput = Target.replace("--url=","")
+print "[ + ] Check the connection with the remote victim..."
+try:
+    GettingAllLinks = scan.getFromURL(urlInput);
+except BaseException,e:
+    print "[ !!! ] Please check again the URL or re-check your internet connection."
+    sys.exit(0)
+
+if TypeMe == "basic":
+    print "[ ~ ] Analayze links on the page, it could take a moment.."
+    GettingAllLinks = scan.getFromURL(urlInput);
+    print "[ ~ ] Okay, we got the links... Start X33er Basic Audit.."
+    for Link in GettingAllLinks:
+        injector.RunAudit(urlInput)
+elif TypeMe == "input":
+    print "[ ~ ] Analayze links on the page, it could take a moment.."
+    GettingAllLinks = scan.getFromURL(urlInput);
+    print "[ ~ ] Okay, we got the links... Start X33er Input Audit.."
+    for v,x in basic.Payloads.iteritems():
+        print "[ ~ ] Testing " + v + "..."
+        injector.RunAuditWithCustomPayload(urlInput,x,basic.PayloadsPCRE[v])
+elif TypeMe == "js":
+    print "[ ~ ] Analayze links on the page, it could take a moment.."
+    GettingAllLinks = scan.getFromURL(urlInput);
+    print "[ ~ ] Okay, we got the links... Start X33er JavaScript Audit.."
+    for v,x in jsfile.Payloads.iteritems():
+        print "[ ~ ] Testing " + v + "..."
+        injector.RunAuditWithCustomPayload(urlInput,x,jsfile.PayloadsPCRE[v])
+else:
+    print "[ ~ ] Analayze links on the page, it could take a moment.."
+    GettingAllLinks = scan.getFromURL(urlInput);
+    print "[ ~ ] Okay, we got the links... Start X33er Basic Audit.."
+    for Link in GettingAllLinks:
+        injector.RunAudit(urlInput)
